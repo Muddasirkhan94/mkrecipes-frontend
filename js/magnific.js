3795aa0ffbbd12617dd393afd3a6d40b335e098e
[Muddasir Khan] <117386294+Muddasirkhan94@users.noreply.github.com>
2023-04-10 16:23:55 +0200

    new

---
+++ js/magnific.js
@@ -0,0 +1,8 @@
+$('.image-gallery').magnificPopup({
+    delegate: 'a', // child items selector, by clicking on it popup will open
+    type: 'image',
+    gallery:{
+        enabled:true
+      }
+    // other options
+  });
